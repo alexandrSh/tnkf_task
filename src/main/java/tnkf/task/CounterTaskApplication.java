30f51171ec02c3c96cd00cc4c48a9fab99c78a09
Aleksandr Sharomov <asharomov@cinimex.ru>
2019-05-29 12:08:03 +0300

    create pom.xml

---
+++ src/main/java/tnkf/task/CounterTaskApplication.java
@@ -0,0 +1,13 @@
+package tnkf.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterTaskApplication.class, args);
+	}
+
+}
